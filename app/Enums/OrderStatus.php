f809a44055668c03d0a4a356b7950175985c2aaa
antonina <atoninasych@gmail.com>
2023-12-27 21:27:42 +0000

    order statuses

--- app/Enums/OrderStatus.php
+++ app/Enums/OrderStatus.php
@@ -6,5 +6,14 @@ enum OrderStatus: string
 {
     case Unpaid = 'unpaid';
     case Paid = 'paid';
+    case Cancelled = 'cancelled';
+    case Shipped = 'shipped';
     case Completed = 'completed';
+
+    public static function getStatuses()
+    {
+        return [
+            self::Paid, self::Unpaid, self::Cancelled, self::Shipped, self::Completed
+        ];
+    }
 }
